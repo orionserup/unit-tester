a3696bb34af6b9c40b12c17039680b7d3c294cfe
Orion Serup <orionserup@gmail.com>
2022-09-15 18:31:26 -0700

    make it actually work

---
+++ test/main/main.c
@@ -0,0 +1,21 @@
+/**
+ * \file main.c
+ * \author Orion Serup (orionserup@gmail.com)
+ * \brief 
+ * \version 0.1
+ * \date 2022-09-14
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include "unity.h"
+
+void app_main(void) {
+
+    UNITY_BEGIN();
+
+    unity_run_all_tests();
+
+    return UNITY_END();
+}
